abcc23f1288e15da4cb7a1c5c337499b9907d6a2
Bharath Upadhya <1992bharath@gmail.com>
2020-05-25 01:24:06 +0530

    Update onExecute.sh

--- steps/sample/1.0/onExecute.sh
+++ steps/sample/1.0/onExecute.sh
@@ -2,7 +2,7 @@ sample() {
   echo foobar
   echo "step name: $step_name"
   fooval=$(find_step_configuration_value foo)
-  echo "value of foo: foo"
+  echo "value of foo: $fooval"
 }
 
 execute_command sample
